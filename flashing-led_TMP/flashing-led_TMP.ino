56de02cde324c0b3b6a61d1a0c0b34c65f7a877f
dex <evilz@bk.ru>
2018-09-05 15:55:16 +0600

    adding fritzing objet

---
+++ flashing-led_TMP/flashing-led_TMP.ino
@@ -0,0 +1,16 @@
+/* мигающий светодиод */
+
+
+int led = 13;
+
+void setup(){
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  digitalWrite(led, HIGH);
+  delay(1000);
+  digitalWrite(led, LOW);
+  delay(1000);
+}
+
