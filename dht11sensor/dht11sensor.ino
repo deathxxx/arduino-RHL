3309712b16d795d15bc68c60c82a3c3cfe729ecc
dex <evilz@bk.ru>
2018-09-05 13:04:31 +0600

    sample shield (10) dht11

--- dht11sensor/dht11sensor.ino
+++ dht11sensor/dht11sensor.ino
@@ -1,27 +1,38 @@
 #include <DHT.h>
-#define DHTPIN 52
 
-DHT dht(DHTPIN, DHT11);
+DHT dht43(43, DHT11);
+DHT dht44(44, DHT11);
+DHT dht45(45, DHT11);
+DHT dht46(46, DHT11);
+DHT dht47(47, DHT11);
+DHT dht48(48, DHT11);
+DHT dht49(49, DHT11);
+DHT dht50(50, DHT11);
+DHT dht51(51, DHT11);
+DHT dht52(52, DHT11);
 
 void setup() {
   Serial.begin(9600);
-  dht.begin();
+  dht52.begin();
 }
 
 void loop() {
 
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
+    float h52 = dht52.readHumidity();
+    float t52 = dht52.readTemperature();
 
-    if (isnan(h) || isnan(t)) {
+    if (isnan(h52) || isnan(t52)) {
       Serial.println("I can not read the testimony");
       return;
     }
+    String out = "d52/h:"+String(h52)+"/t/"+String(t52);
+    Serial.println(out);
+    
     Serial.print("Humidity - ");
-    Serial.print(h);
+    Serial.print(h52);
     Serial.print("   ");
     Serial.print("Temperature - ");
-    Serial.print(t);
+    Serial.print(t52);
     Serial.print("\n");
 
     delay(1000);
